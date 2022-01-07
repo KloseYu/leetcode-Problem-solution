176ee6394a2119c6c395770d32ac384a3dcb0f66
klose1178 <62323013+klose1178@users.noreply.github.com>
2022-01-07 21:08:06 +0800

    整数反转Kotlin版

--- intreverse.kt
+++ intreverse.kt
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     fun reverse(x: Int): Int {
         var y = x
         var r = 0
@@ -10,6 +10,21 @@ class Solution {
             r = r * 10 + p
             y /= 10
         }
+        return r
+    }
+}*/
+class Solution {
+    fun reverse(x: Int): Int {
+        var r = 0
+        var y = x
+        while (y != 0) {
+             if (r <Int.MIN_VALUE / 10 || r > Int.MAX_VALUE / 10) {
+            return 0
+        }
+        val digit = y % 10
+        y /= 10
+        r = r * 10 + digit
+        }    
         return r
     }
 }
