5edb934996309de2146dd431c93b1271a47402f0
klose1178 <62323013+klose1178@users.noreply.github.com>
2022-01-07 21:37:53 +0800

    括号的最大嵌套深度Kotlin版

---
+++ maxDepth.kt
@@ -0,0 +1,30 @@
+class Solution {
+    fun maxDepth(s: String): Int {
+          var ans = 0
+          var size = 0
+        for (i in 0 until s.count()) {
+            val ch = s[i]
+            if (ch == '(') {
+                ++size
+                ans = Math.max(ans, size)
+            } else if (ch == ')') {
+                --size
+            }
+        }
+        return ans
+    }
+}
+/*class Solution {
+    fun maxDepth(s: String): Int {
+        var max = 0
+        var current = 0
+        s.forEach {
+            when (it) {
+                '(' -> current ++
+                ')' -> current --
+            }
+            max = Math.max(current, max)
+        }
+        return max
+    }
+}*/
