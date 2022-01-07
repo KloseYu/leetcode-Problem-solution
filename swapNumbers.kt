9799680c5e421c7f44c4302567286983c7ead629
klose1178 <62323013+klose1178@users.noreply.github.com>
2022-01-07 19:21:26 +0800

    异或交换数字kotlin版

--- swapNumbers.kt
+++ swapNumbers.kt
@@ -1,6 +1,6 @@
 class Solution {
     fun swapNumbers(numbers: IntArray): IntArray {
-         numbers[0] = numbers[0] xor numbers[1]
+         numbers[0]= numbers[0] xor numbers[1]
          numbers[1] = numbers[1] xor numbers[0]
          numbers[0] = numbers[0] xor numbers[1]
          return numbers
