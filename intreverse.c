431bf60acca6b043cf73fb930feec4063418c39f
klose1178 <62323013+klose1178@users.noreply.github.com>
2022-01-07 20:54:12 +0800

    整数反转C语言版

--- intreverse.c
+++ intreverse.c
@@ -9,3 +9,15 @@ int reverse(int x)
     else if((num<INT_MIN/10)||((num==INT_MIN/10)&&(x<-8)))  return 0;
     else    return num*10+x;
 }
+/*int reverse(int x) {
+    int rev = 0;
+    while (x != 0) {
+        if (rev < INT_MIN / 10 || rev > INT_MAX / 10) {
+            return 0;
+        }
+        int digit = x % 10;
+        x /= 10;
+        rev = rev * 10 + digit;
+    }
+    return rev;
+}*/
