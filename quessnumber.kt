05b2e0b88a2ad1faf0b59925f1fda833e794fd82
klose1178 <62323013+klose1178@users.noreply.github.com>
2022-01-07 21:29:51 +0800

    猜数字大小Kotlin版

--- quessnumber.kt
+++ quessnumber.kt
@@ -22,3 +22,29 @@ class Solution:GuessGame() {
         return left
     }
 }
+
+/** 
+ * The API guess is defined in the parent class.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ * fun guess(num:Int):Int {}
+ */
+
+/*class Solution:GuessGame() {
+    override fun guessNumber(n:Int):Int {
+        var left = 1
+        var right = n
+        while (left < right) { // 循环直至区间左右端点相同
+            val mid = left + (right - left) / 2// 防止计算时溢出
+            if (guess(mid) <= 0) {
+                right = mid // 答案在区间 [left, mid] 中
+            } else {
+                left = mid + 1 // 答案在区间 [mid+1, right] 中
+            }
+        }
+        // 此时有 left == right，区间缩为一个点，即为答案
+        return left
+    }
+}*/
