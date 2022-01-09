96e7cacb33fdda4e9dd7304f7c8af1b9c1c02f4e
klose1178 <62323013+klose1178@users.noreply.github.com>
2022-01-09 13:45:45 +0800

    pow(x,n)Kotlin版

--- myPow.kt
+++ myPow.kt
@@ -2,7 +2,7 @@ class Solution {
     fun myPow(x: Double, n: Int): Double {
         var varx = x
         var varn = n
-        if(varn==0)  return 1.0
+        if (varn==0) 1.0
         else if (varn < 0) {
             varx = 1 / varx
             varn = -varn
@@ -30,7 +30,7 @@ class Solution {
     fun myPow(x: Double, n: Int): Double {
         var varx = x
         var varn = n
-        if(varn==0)  1.0
+        if (varn==0)  1.0
         else if (varn < 0) {
             varx = 1 / varx
             varn = Math.abs(varn)
