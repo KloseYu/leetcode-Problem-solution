1a818c39d2f6f1270d0acba2d0b14127120fb8b4
klose1178 <62323013+klose1178@users.noreply.github.com>
2022-01-17 11:07:15 +0800

    丑数Kotlin版

--- isUgly.kt
+++ isUgly.kt
@@ -4,17 +4,13 @@ class Solution {
             0-> return false
             1,2,3,5-> return true
         }
-
-        var ans = n
-        
+          var ans = n
           while (0 == ans % 2)  ans /= 2
           while  (0 == ans % 3)  ans /= 3
           while  (0 == ans % 5)  ans /= 5     
-        
-        return ans == 1
+          return ans == 1
     }
 }
-
 /*
 class Solution {
     fun isUgly(n: Int): Boolean {
