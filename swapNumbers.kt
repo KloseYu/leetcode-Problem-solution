94d6b5bd9737c860595090bd6a1986f5997d27b5
klose1178 <62323013+klose1178@users.noreply.github.com>
2022-01-23 20:43:29 +0800

    交换数字Kotlin版

--- swapNumbers.kt
+++ swapNumbers.kt
@@ -6,3 +6,18 @@ class Solution {
          return numbers
     }
 }
+/*class Solution {
+    fun swapNumbers(numbers: IntArray): IntArray {
+      numbers[0] = numbers[1].also {
+            numbers[1] = numbers[0]
+        }
+        return numbers
+    }
+}*/
+/*
+class Solution {
+    fun swapNumbers(numbers: IntArray): IntArray {
+         return intArrayOf (numbers[1],numbers[0])
+    }
+}*/
+
