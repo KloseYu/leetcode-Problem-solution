0489237e7a4a4a10bf4601585e9140c9836edcd2
klose1178 <62323013+klose1178@users.noreply.github.com>
2022-02-02 14:12:46 +0800

    交换数字Scala版

--- swapNumbers.scala
+++ swapNumbers.scala
@@ -1,5 +1,11 @@
 object Solution {
     def swapNumbers(numbers: Array[Int]): Array[Int] = {
         Array(numbers(1), numbers(0))
+        /*
+        numbers(0)^=   numbers(1)
+        numbers(1)^=   numbers(0)
+        numbers(0)^=   numbers(1)
+        numbers
+        */
     }
 }
