8c11523de2a1dadb189d1aebc205cb2c2c5b12af
klose1178 <62323013+klose1178@users.noreply.github.com>
2022-02-10 20:43:38 +0800

    最简分数Kotlin版

--- simplifiedFractions.kt
+++ simplifiedFractions.kt
@@ -14,6 +14,34 @@
     fun gcd(a: Int, b: Int): Int {
         return if (b != 0) gcd(b, a % b) else a
     }
+    
+    /*fun simplifiedFractions(n: Int): List<String> {
+        if (n == 1) {
+            return ArrayList()
+        }
+        val list = ArrayList<String>()
+        for (i in 2..n) {
+            for (j in 1 until i) {
+                if (gcd(j, i) != -1) {
+                    continue
+                }
+                list.add("$j/$i")
+            }
+        }
+        return list
+    }
+
+    fun gcd(m: Int, n: Int): Int {
+        var t = 2
+        while (t <= m && t <= n) {
+            if (m % t == 0 && n % t == 0) {
+                return t
+            }
+            t++
+        }
+        return -1
+    }*/
+
 }
 
 
