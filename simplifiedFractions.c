a02b89349209b44cfa2af449402a6203beace341
klose1178 <62323013+klose1178@users.noreply.github.com>
2022-02-10 20:51:09 +0800

    最简分数C语言版

---
+++ simplifiedFractions.c
@@ -0,0 +1,22 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+#define MAX_FRACTION_LEN 10
+int gcd(int a, int b) {
+   if (b != 0 ) return gcd(b, a % b) ;  else return a;
+}
+char ** simplifiedFractions(int n, int* returnSize) {
+    char ** ans = (char **)malloc(sizeof(char *) * n * (n - 1) / 2 );
+    int pos = 0;
+    for (int denominator = 2; denominator <= n; denominator++) {
+        for (int numerator = 1; numerator < denominator; numerator++) {
+            if (gcd(numerator, denominator) == 1) {
+                ans[pos] = (char *)malloc(sizeof(char) * MAX_FRACTION_LEN);
+                snprintf(ans[pos++], MAX_FRACTION_LEN, "%d%c%d", numerator, '/', denominator);
+            }
+        }
+    }
+    *returnSize = pos;
+    return ans;
+}
+
