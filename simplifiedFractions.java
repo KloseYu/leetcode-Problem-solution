e2607ab87cd960fdde1c9e6800d68530bdd80ab0
klose1178 <62323013+klose1178@users.noreply.github.com>
2022-02-10 20:47:19 +0800

    最简分数Java版

---
+++ simplifiedFractions.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<String> simplifiedFractions(int n) {
+        List<String> ans = new ArrayList<String>();
+        for (int denominator = 2; denominator <= n; ++denominator) {
+            for (int numerator = 1; numerator < denominator; ++numerator) {
+                if (gcd(numerator, denominator) == 1) {
+                    ans.add(numerator + "/" + denominator);
+                }
+            }
+        }
+        return ans;
+    }
+
+    public int gcd(int a, int b) {
+        //if (b != 0 ) return gcd(b, a % b) ;  else return a;
+         return b != 0 ? gcd(b, a % b) : a;
+    }
+}
