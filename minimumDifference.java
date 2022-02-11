15fdb789958bfd89f116ae7cd1f003a3b6bc86f1
klose1178 <62323013+klose1178@users.noreply.github.com>
2022-02-11 13:32:45 +0800

    学生分数的最小差值Java版

--- minimumDifference.java
+++ minimumDifference.java
@@ -8,5 +8,16 @@ class Solution {
         }
         return ans;
     }
+     /*public int minimumDifference(int[] nums, int k) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        int ans = Integer.MAX_VALUE;
+        int i=0;
+        while(i + k - 1 < n) {
+            ans = Math.min(ans, nums[i + k - 1] - nums[i]);
+            i++;
+        }
+        return ans;
+    }*/
 }
 
