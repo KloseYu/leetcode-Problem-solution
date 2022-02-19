21527e196dfea28d7ab1c09ace3f0c4bd8bbca56
klose1178 <62323013+klose1178@users.noreply.github.com>
2022-02-19 18:53:55 +0800

    煎饼排序C语言版

---
+++ pancakeSort.c
@@ -0,0 +1,33 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+void reverse(int *arr, int arrSize) {
+    for (int left = 0, right = arrSize - 1; left < right; left++, right--) {
+            arr[left] ^=  arr[right];
+            arr[right] ^=  arr[left];
+            arr[left] ^= arr[right];
+    }
+}
+
+int *pancakeSort(int *arr, int arrSize, int *returnSize){
+    int *ret = (int *)malloc(sizeof(int) * (arrSize - 1) * 2);
+    int retSize = 0;
+    for (int n = arrSize; n > 1; n--) {
+        int index = 0;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] >= arr[index]) {
+                index = i;
+            }
+        }
+        if (index == n - 1) {
+            continue;
+        }
+        reverse(arr, index + 1);
+        reverse(arr, n);
+        ret[retSize++] = index + 1;
+        ret[retSize++] = n;
+    }
+    *returnSize = retSize;
+    return ret;
+}
+
