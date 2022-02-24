9fcf70c7922168424dc58629ba75b83889056824
klose1178 <62323013+klose1178@users.noreply.github.com>
2022-02-24 17:19:43 +0800

    2的幂Kotlin版

--- isPowerOfTwo.kt
+++ isPowerOfTwo.kt
@@ -1,6 +1,7 @@
 class Solution {
-    val big = 1.shl(30)
+    //val big = 1.shl(30)
     fun isPowerOfTwo(n: Int): Boolean {
-       return n > 0 && big % n == 0
+      // return n > 0 && big % n == 0
+      return    n > 0 && (n and (n - 1)) == 0
     }
 }
