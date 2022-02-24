eb36a96343d6075c8ca7470f162dd07757c1c45d
klose1178 <62323013+klose1178@users.noreply.github.com>
2022-02-24 11:58:15 +0800

    球会传何处Kotlin版

--- findBall.kt
+++ findBall.kt
@@ -1,23 +1,24 @@
+import java.util.*
 class Solution {
-    public int[] findBall(int[][] grid) {
-        int n = grid[0].length;
-        int ans[] = new int[n];
-        Arrays.fill(ans, -1);
-        for (int j = 0; j < n; j++) {
-            int col = j;  // 球的初始列
-            for (int[] row : grid) {
-                int dir = row[col];
-                col += dir;  // 移动球
+    fun findBall(grid: Array<IntArray>): IntArray {
+        val n = grid[0].size
+        val ans = IntArray(n)
+        Arrays.fill(ans, -1)
+        for (j in 0 until n) {
+            var col = j // 球的初始列
+            for (row in grid) {
+                val dir = row[col]
+                col += dir // 移动球
                 if (col < 0 || col == n || row[col] != dir) {  // 到达侧边或 V 形
-                    col = -1;
-                    break;
+                    col = -1
+                    break
                 }
             }
             if (col >= 0) {  // 成功到达底部
-                ans[j] = col;
+                ans[j] = col
             }
         }
-        return ans;
+        return ans
     }
 }
 
