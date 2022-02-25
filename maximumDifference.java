76b437086cbd6291e12d83ab1a9e767c948e89cb
klose1178 <62323013+klose1178@users.noreply.github.com>
2022-02-26 07:16:03 +0800

    增量元素之间的最大差值Java版

---
+++ maximumDifference.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maximumDifference(int[] nums) {
+    int ans = -1, premin = nums[0];
+     for (int i = 1; i < nums.length; ++i) 
+     { if (nums[i] > premin) 
+     {
+          ans = Math.max(ans, nums[i] - premin);
+           } else { premin = nums[i]; 
+           }  } return ans;
+    }
+}
