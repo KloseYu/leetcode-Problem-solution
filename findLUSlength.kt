9df9cbe9187222cdcdf700f3fbe3fb531ffbe245
klose1178 <62323013+klose1178@users.noreply.github.com>
2022-03-05 07:49:47 +0800

    最长特殊序列 ⅠKotlin版

--- findLUSlength.kt
+++ findLUSlength.kt
@@ -1,5 +1,6 @@
 class Solution {
     fun findLUSlength(a: String, b: String): Int {
         return if (a != b) Math.max(a.length, b.length) else -1 
+        //return if (a != b) a.length.coerceAtLeast(b.length) else -1
     }
 }
